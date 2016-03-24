b6c5129f89078c6470a200f929d74ddc63a90fd3
Scott Lemmon <scott@authentise.com>
2016-03-24 11:42:20 -0600

    Fix logic in handling expected execption which doesn't raise
    The stack trace is getting swollowed for these errors, which is
    unforunate.  I will hopefully be able to fix this later.
    
    AE-1624 #time 3h

--- pytest_raises/pytest_raises.py
+++ pytest_raises/pytest_raises.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
+import sys
 
 import pytest
+
+class ExpectedException(Exception):
+    pass
 
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_call(item):
@@ -8,11 +12,18 @@ def pytest_runtest_call(item):
     raises_marker = item.get_marker('raises')
     if raises_marker:
         exception = raises_marker.kwargs.get('exception')
+        exception = exception or Exception
 
-        if exception:
+        raised_exception = outcome.excinfo[1] if outcome.excinfo else None
+        traceback = outcome.excinfo[2] if outcome.excinfo else None
+        if isinstance(raised_exception, exception):
+            outcome.force_result(None)
+        else:
             try:
-                outcome.get_result()
-            except (exception):
-                pass
-
-        outcome.force_result(True)
+                raise ExpectedException('Expected exception {}, but it did not raise'.format(exception))
+            except(ExpectedException) as ex:
+                excinfo = sys.exc_info()
+                if traceback:
+                    outcome.excinfo = excinfo[:2] + (traceback, )
+                else:
+                    outcome.excinfo = excinfo

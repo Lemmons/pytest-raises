2aa0233f4b52d08be07e93f8edf4ece45361d7dd
Scott Lemmon <scott@authentise.com>
2016-05-25 21:26:17 -0600

    Add test test helper for running a test of tests
    Again, very meta.  This test helper takes care of most of the boiler
    plat for running these tests of test.  See the updated tests for details
    on it usage.
    
    This also contains a little bit of test renaming.

--- tests/test_raises.py
+++ tests/test_raises.py
@@ -1,145 +1,131 @@
 # -*- coding: utf-8 -*-
 
+def _run_tests_test(testdir, code, expected_output, expcted_return_code):
+    testdir.makepyfile(code)
+    result = testdir.runpytest('-v')
+    result.stdout.fnmatch_lines(expected_output)
+    assert result.ret == expcted_return_code
 
 def test_pytest_mark_raises_expected_exception(testdir):
-    testdir.makepyfile("""
-        import pytest
+    _run_tests_test(testdir, """
+            import pytest
 
-        class SomeException(Exception):
-            pass
+            class SomeException(Exception):
+                pass
 
-        @pytest.mark.raises(exception = SomeException)
-        def test_mark_raises_expected_exception():
-            raise SomeException('the message')
-    """)
-
-    result = testdir.runpytest('-v')
-
-    result.stdout.fnmatch_lines([
-        '*::test_mark_raises_expected_exception PASSED',
-    ])
-
-    assert result.ret == 0
+            @pytest.mark.raises(exception = SomeException)
+            def test_mark_raises_expected_exception():
+                raise SomeException('the message')
+        """,
+        [
+            '*::test_mark_raises_expected_exception PASSED',
+        ],
+        0
+    )
 
 def test_mark_raises_no_args(testdir):
-    testdir.makepyfile("""
-        import pytest
+    _run_tests_test(testdir, """
+            import pytest
 
-        class AnotherException(Exception):
-            pass
+            class AnotherException(Exception):
+                pass
 
-        @pytest.mark.raises()
-        def test_mark_raises_no_args():
-            raise AnotherException('the message')
-    """)
-
-    result = testdir.runpytest('-v')
-
-    result.stdout.fnmatch_lines([
-        '*::test_mark_raises_no_args PASSED',
-    ])
-
-    assert result.ret == 0
+            @pytest.mark.raises()
+            def test_mark_raises_no_args():
+                raise AnotherException('the message')
+        """,
+        [
+            '*::test_mark_raises_no_args PASSED',
+        ],
+        0
+    )
 
 def test_unmarked_test(testdir):
-    testdir.makepyfile("""
-        import pytest
+    _run_tests_test(testdir, """
+            import pytest
 
-        class SomeException(Exception):
-            pass
+            class SomeException(Exception):
+                pass
 
-        def test_unmarked_test():
-            raise SomeException('the message')
-    """)
-
-    result = testdir.runpytest('-v')
-
-    result.stdout.fnmatch_lines([
-        '*::test_unmarked_test FAILED',
-    ])
-
-    assert result.ret == 1
+            def test_unmarked_test():
+                raise SomeException('the message')
+        """,
+        [
+            '*::test_unmarked_test FAILED',
+        ],
+        1
+    )
 
 def test_pytest_mark_raises_no_exception(testdir):
-    testdir.makepyfile("""
-        import pytest
+    _run_tests_test(testdir, """
+            import pytest
 
-        class SomeException(Exception):
-            pass
+            class SomeException(Exception):
+                pass
 
-        @pytest.mark.raises(exception = SomeException)
-        def test_pytest_mark_raises_no_exception():
-            pass
-    """)
+            @pytest.mark.raises(exception = SomeException)
+            def test_pytest_mark_raises_no_exception():
+                pass
+        """,
+        [
+            '*::test_pytest_mark_raises_no_exception FAILED',
+            "*Expected exception <class '*.SomeException'>, but it did not raise",
+        ],
+        1
+    )
 
-    result = testdir.runpytest('-v')
+def test_pytest_mark_raises_unexpected_exception(testdir):
+    _run_tests_test(testdir, """
+            import pytest
 
-    result.stdout.fnmatch_lines([
-        '*::test_pytest_mark_raises_no_exception FAILED',
-        "*Expected exception <class '*.SomeException'>, but it did not raise",
-    ])
+            class SomeException(Exception):
+                pass
 
-    assert result.ret == 1
+            class AnotherException(Exception):
+                pass
 
-def test_pytest_mark_raises_pass_through_unexpected_exception(testdir):
-    testdir.makepyfile("""
-        import pytest
-
-        class SomeException(Exception):
-            pass
-
-        class AnotherException(Exception):
-            pass
-
-        @pytest.mark.raises(exception = SomeException)
-        def test_pytest_mark_raises_pass_through_unexpected_exception():
-            raise AnotherException('the message')
-
-    """)
-
-    result = testdir.runpytest('-v')
-
-    result.stdout.fnmatch_lines([
-        '*::test_pytest_mark_raises_pass_through_unexpected_exception FAILED',
-        '*test_pytest_mark_raises_pass_through_unexpected_exception.AnotherException: the message',
-    ])
-
-    assert result.ret == 1
+            @pytest.mark.raises(exception = SomeException)
+            def test_pytest_mark_raises_unexpected_exception():
+                raise AnotherException('the message')
+        """,
+        [
+            '*::test_pytest_mark_raises_unexpected_exception FAILED',
+            '*test_pytest_mark_raises_unexpected_exception.AnotherException: the message',
+        ],
+        1
+    )
 
 def test_pytest_mark_raises_parametrize(testdir):
-    testdir.makepyfile("""
-        import pytest
+    _run_tests_test(testdir, """
+            import pytest
 
-        class SomeException(Exception):
-            pass
+            class SomeException(Exception):
+                pass
 
-        class AnotherException(Exception):
-            pass
+            class AnotherException(Exception):
+                pass
 
-        @pytest.mark.parametrize('error', [
-            None,
-            pytest.mark.raises(SomeException('the message'), exception=SomeException),
-            pytest.mark.raises(AnotherException('the message'), exception=AnotherException),
-            pytest.mark.raises(Exception('the message')),
-            pytest.mark.raises(AnotherException('the message'), exception=SomeException),
-            SomeException('the message'),
-            pytest.mark.raises(None, exception=SomeException),
-        ])
-        def test_mark_raises(error):
-            if error:
-                raise error
-    """)
-
-    result = testdir.runpytest('-v')
-
-    result.stdout.fnmatch_lines([
-        '*::test_mark_raises*None* PASSED',
-        '*::test_mark_raises*error1* PASSED',
-        '*::test_mark_raises*error2* PASSED',
-        '*::test_mark_raises*error3* PASSED',
-        '*::test_mark_raises*error4* FAILED',
-        '*::test_mark_raises*error5* FAILED',
-        '*::test_mark_raises*6None* FAILED',
-    ])
-
-    assert result.ret == 1
+            @pytest.mark.parametrize('error', [
+                None,
+                pytest.mark.raises(SomeException('the message'), exception=SomeException),
+                pytest.mark.raises(AnotherException('the message'), exception=AnotherException),
+                pytest.mark.raises(Exception('the message')),
+                pytest.mark.raises(AnotherException('the message'), exception=SomeException),
+                SomeException('the message'),
+                pytest.mark.raises(None, exception=SomeException),
+            ])
+            def test_mark_raises(error):
+                if error:
+                    raise error
+        """,
+        [
+            '*::test_mark_raises*None* PASSED',
+            '*::test_mark_raises*error1* PASSED',
+            '*::test_mark_raises*error2* PASSED',
+            '*::test_mark_raises*error3* PASSED',
+            '*::test_mark_raises*error4* FAILED',
+            '*::test_mark_raises*error5* FAILED',
+            '*::test_mark_raises*6None* FAILED',
+        ],
+        1
+    )
